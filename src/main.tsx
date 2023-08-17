f25a47c8ad7aaa0a968b643de8a296873f5a066e
Luca Palmarucci <lpalmarucci@gmail.com>
2023-08-17 22:04:21 +0200

    Moved CssBaseline

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App.tsx";
 import "./index.css";
-import ToggleColorModeContext from "./context/ColorModeContext.tsx";
-import { CssBaseline } from "@material-ui/core";
+import ToggleColorModeContext from "./context/ColorMode/ColorModeContext.tsx";
+import "mapbox-gl/dist/mapbox-gl.css";
 
 ReactDOM.render(
   <React.StrictMode>
     <ToggleColorModeContext>
-      <CssBaseline />
       <App />
     </ToggleColorModeContext>
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById("root"),
 );
